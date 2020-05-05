9162f85ed4724de43b8007844d2bca4394e945c4
Nirav Patel <niravkpatel28@gmail.com>
2020-05-05 23:35:30 +0530

    Updates

---
+++ precache-manifest.a14bf6526be9f93d6c2d57f0e893a18c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abedb16a7193e138a52ba6a60f1fa4f",
+    "url": "/React-Counter/index.html"
+  },
+  {
+    "revision": "9a4ec2998e5daa21cc43",
+    "url": "/React-Counter/static/css/main.0a3eb81e.chunk.css"
+  },
+  {
+    "revision": "36337369e118c5696805",
+    "url": "/React-Counter/static/js/2.0a1af41e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Counter/static/js/2.0a1af41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4ec2998e5daa21cc43",
+    "url": "/React-Counter/static/js/main.438e7a72.chunk.js"
+  },
+  {
+    "revision": "167a5fd54a516ffbe9f7",
+    "url": "/React-Counter/static/js/runtime-main.846754e3.js"
+  }
+]);
